3674902ca1baf3b7dc42757ebd3dec932d374cdf
aedelgado19 <aedelgado19@gmail.com>
2020-10-11 18:17:19 -0700

    initial commit

---
+++ Classes.cpp
@@ -0,0 +1,15 @@
+/* Author: Allison Delgado
+ * Last updated: October 11
+ * Classes is a project that serves as a media database holding music vids,
+ * movies, and songs to practice using classes and inheritance.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+  return 0;
+}

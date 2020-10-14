9e2cbc7912758e896fed94fa6643695d4987145d
aedelgado19 <aedelgado19@gmail.com>
2020-10-13 18:01:31 -0700

    wrote function main

--- Classes.cpp
+++ Classes.cpp
@@ -5,11 +5,67 @@
  */
 
 #include <iostream>
+#include "ClassesHeader.h"
 using namespace std;
 
+class Parent{
+  public:
+  int getYear();
+  char* getTitle();
+  
+};
+
+int Parent::getYear(){
+
+}
+
+char* Parent::getTitle(){
+
+}
+
+bool checkLegal(char *command){
+  if(strcmp(command, "ADD") == 0){
+    return true;
+  }
+  if(strcmp(command, "DELETE") == 0){
+    return true;
+  }
+  if(strcmp(command, "QUIT") == 0){
+    return true;
+  }
+  if(strcmp(command, "SEARCH") == 0){
+    return true;
+  }
+  return false;
+}
+
 int main(){
+  cout << "Welcome to the media database."
+  
+  bool isLegal = checkLegal(command);
 
-
-
+  while (strcmp(command, "QUIT") != 0){
+    cout << "Enter a command: ADD, SEARCH, DELETE, QUIT" << endl;
+    char command[7];
+    cin.get(command, 7);
+    cin.get();
+    //format to all uppercase
+    for(int i = 0; i < strlen(command); i++){
+      command[i] = toupper(command[i]);
+    }
+    //check if legal
+    if(isLegal == true){
+      
+      if(strcmp(command, "ADD") == 0){
+	
+      }
+      if(strcmp(command, "SEARCH") == 0){
+	
+      }
+      if(strcmp(command, "DELETE") == 0){
+	
+      }
+    }
+  }
   return 0;
 }
